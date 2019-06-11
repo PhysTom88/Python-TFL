63c7f22c5ff5b0feac1356c3e1f61b1caa7a900e
PhysTom88 <tom.ravenscroft@uktv.co.uk>
2019-06-11 11:44:15 +0100

    added a method for getting arrival predictions

---
+++ tfl/api/mode.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from tfl.api import Api
+from tfl.api.line import Line
+from tfl.api.exceptions import InvalidInputError
+from tfl.lib.validation import validate_input
+
+
+class Mode(Api):
+
+    def mode_arrival_predictions(self, mode, count=None):
+        url = self.base_url + "Mode/{0}/Arrivals/"
+        modes = [m["modeName"] for m in Line(app_id=self.app_id, app_key=self.app_key).valid_modes()]
+
+        if mode in modes:
+            extra_params = {}
+            if count is not None:
+                extra_params["count"] = validate_input(count, int, "count")
+
+            response = self._request(url.format(mode), extra_params=extra_params, http_method="GET")
+            data = self._check_response(response.json())
+
+            return data
+        else:
+            raise InvalidInputError("'{0}' not a valid mode".format(mode))
